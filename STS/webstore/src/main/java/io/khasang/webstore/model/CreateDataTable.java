bc2ee2e148331a1f62abc783a7c9fa171962ddf7
Vladimir Makarov <mrArxi@gmail.com>
2016-02-16 20:28:03 +0300

    all update v.makarov

---
+++ STS/webstore/src/main/java/io/khasang/webstore/model/CreateDataTable.java
@@ -0,0 +1,39 @@
+package io.khasang.webstore.model;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+public class CreateDataTable {
+    public static String sqlCheck;
+
+    public void sqlInsert() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+        dataSource.setUsername("root");
+        dataSource.setUrl("jdbc:mysql://localhost/webstore");
+        dataSource.setPassword("root");
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        System.out.println("try to update db...");
+        try {
+            System.out.println("Creating tables");
+            /*jdbcTemplate.execute("DROP TABLE IF EXISTS products");
+            jdbcTemplate.execute("create table products(ID INT NOT NULL,"
+                    + " pName MEDIUMTEXT NOT NULL, description LONGTEXT)");*/
+//            jdbcTemplate.update("INSERT INTO products(ID, pName, description) VALUES(1, 'apple', 'red')");
+//            jdbcTemplate.update("INSERT INTO products(ID, pName, description) VALUES(2, 'banan', 'yellow')");
+//            jdbcTemplate.update("INSERT INTO products(ID, pName, description) VALUES(3, 'bread', null)");
+            jdbcTemplate.update("INSERT INTO products(ID, pName, description) VALUES(4, 'milk', 'natural')");
+            jdbcTemplate.update("INSERT INTO products(ID, pName, description) VALUES(5, 'becon', null)");
+            jdbcTemplate.update("INSERT INTO products(ID, pName, description) VALUES(6, 'bread', 'black')");
+            sqlCheck = "db updated";
+        } catch (Exception e) {
+            sqlCheck = "Have error: " + e;
+            System.err.println(sqlCheck);
+        }
+    }
+
+    public String sqlInsertCheck() {
+        CreateDataTable sql = new CreateDataTable();
+        sql.sqlInsert();
+        return sqlCheck;
+    }
+}
