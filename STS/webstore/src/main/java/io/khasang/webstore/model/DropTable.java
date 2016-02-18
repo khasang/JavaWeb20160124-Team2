bdbce2126b9b776e19db6e37af468b7c58ad3005
Veselina Zatchepina <techpro20@mail.ru>
2016-02-19 01:23:55 +0300

    all function with DB placed in DropTable.java

--- STS/webstore/src/main/java/io/khasang/webstore/model/DropTable.java
+++ STS/webstore/src/main/java/io/khasang/webstore/model/DropTable.java
@@ -3,33 +3,110 @@ package io.khasang.webstore.model;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DropTable extends HttpServlet {
 
+    private SimpleDriverDataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
+    private List<String> listOfNameTables = new ArrayList<String>();
+    private List dataList = new ArrayList();
+
     public static String sqlCheck;
     public String nameTable;
+    private String page = "/WEB-INF/views/drop.jsp";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Connect to database
+        connectionToDB();
+        //Create table with all names of tables in database
+        sqlInsertCheck();
         resp.setContentType("text/html;charset=UTF-8");
-        req.getRequestDispatcher("/WEB-INF/views/drop.jsp").include(req, resp);
-        nameTable = req.getParameter("name");
+        //Getting name of all tables from table "allTableDB"
+        getNameOfTableFromTable(req, resp);
+        listOfNameTables.clear();
+        dataList.clear();
+        //Getting input user value in "text place"
+        String currentValueSet = req.getParameter("namet");
+        String currentValueText = req.getParameter("name");
+        if (!currentValueSet.equals("Select table")) {
+            nameTable = currentValueSet;
+        } else if (currentValueText != null){
+            nameTable = currentValueText;
+        }
+        //Remove input value
         dropSql();
     }
 
-    public void dropSql() {
-        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+    public void connectionToDB() {
+        dataSource = new SimpleDriverDataSource();
         dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
         dataSource.setUsername("root");
         dataSource.setUrl("jdbc:mysql://localhost/webstore");
         dataSource.setPassword("root");
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        System.out.println("try to update db...");
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        System.out.println("Try to connect to database...");
+    }
+
+    public String sqlInsertCheck() {
+        try {
+            DatabaseMetaData metaData = jdbcTemplate.getDataSource().getConnection().getMetaData();
+            ResultSet resultSet = metaData.getTables("webstore", null, null, null);
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(3));
+                listOfNameTables.add(resultSet.getString(3));
+            }
+            resultSet.close();
+            System.out.println("Creating tables...");
+            jdbcTemplate.execute("DROP TABLE IF EXISTS allTableDB");
+            jdbcTemplate.execute("create table allTableDB(Id INT NOT NULL,"
+                    + " tName LONGTEXT NOT NULL, description MEDIUMTEXT NULL)");
+            for (int i=0; i<listOfNameTables.size(); i++) {
+                jdbcTemplate.update("INSERT INTO allTableDB(Id, tName, description) VALUES("
+                        + i +", " +"'"+listOfNameTables.get(i)+"'" + ", " + "'table')");
+            }
+            sqlCheck = "Database updated";
+            System.out.println(sqlCheck);
+        } catch (Exception e) {
+            sqlCheck = "Have error: " + e;
+            System.err.println(sqlCheck);
+        }
+        return sqlCheck;
+    }
+
+    public void getNameOfTableFromTable(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ResultSet set;
+
+        try {
+            String sql = "SELECT * from allTableDB";
+            Statement statement = jdbcTemplate.getDataSource().getConnection().createStatement();
+            statement.executeQuery(sql);
+            set = statement.getResultSet();
+            while (set.next()) {
+                dataList.add(set.getString("tName"));
+            }
+            set.close();
+            statement.close();
+        } catch (Exception e) {
+            System.out.println("Exception is ;" + e);
+        }
+        req.setAttribute("data", dataList);
+        RequestDispatcher dispatcher = req.getRequestDispatcher(page);
+        if (dataList != null) {
+            dispatcher.forward(req, resp);
+        }
+    }
+
+    public void dropSql() {
         try {
             System.out.println("Dropping tables");
             System.out.println(nameTable);
@@ -40,5 +117,7 @@ public class DropTable extends HttpServlet {
             System.err.println(sqlCheck);
         }
     }
+
+
 
 }
--- STS/webstore/web/WEB-INF/views/drop.jsp
+++ STS/webstore/web/WEB-INF/views/drop.jsp
@@ -1,3 +1,4 @@
+<%@ page import="java.util.Iterator" %>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%--
   Created by IntelliJ IDEA.
@@ -6,7 +7,7 @@
   Time: 11:55
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" import="java.util.*"  %>
 <head>
     <title>Drop Table</title>
     <link rel="stylesheet" href="/css/styleDrop.css">
@@ -16,12 +17,21 @@
     <input class="menuButton" type="button" onclick="history.back();" value="Back to menu">
     <input class="logoutButton" type="button" onclick="" value="Log out">
     <center><p><b>Введите название таблицы для удаления:</b></p>
-
             <form action="drop" method="GET">
             <input class="placeForInput" type="text" size="40" name="name" id="name">
+                <p><b>Или выберите из списка:</b></p>
+                <p><center><select required class="selectButton"  name="namet" id="namet">
+                <option>Select table</option>
+                <%Iterator itr;%>
+                <%List data = (List)request.getAttribute("data");
+                    for(itr=data.iterator();itr.hasNext();) {
+                %>
+                <option><%=itr.next()%></option>
+                <%}%>
+            </select></center></p>
             <p><center><input class="dropButton" type="submit" value="Drop" ></center></p>
+                <p><h6>Обновите страницу после нажатия кнопки</h6></p>
             </form>
-
     </center>
 </body>
 </html>
