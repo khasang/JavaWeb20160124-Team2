ee462d425e09081f8a59e942a5b8e1c85fb3fc2b
ilnur serbaev <serbaevilnur@gmail.com>
2016-04-20 22:39:48 +0500

    remove files not related to Webstore

--- webWithSecurityHW/src/main/java/io/khasang/web/config/SecurityInit.java
+++
@@ -1,7 +0,0 @@
-package io.khasang.web.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer{
-
-}
