e5865de43673a5b8d8db5c03cf23b72032a85af9
ilnur serbaev <serbaevilnur@gmail.com>
2016-04-24 17:45:36 +0500

    switch to simpledriverdatasource

--- STS/webstore/src/main/java/io/khasang/webstore/config/JavaConfig.java
+++ STS/webstore/src/main/java/io/khasang/webstore/config/JavaConfig.java
@@ -26,85 +26,85 @@ import java.util.Properties;
 @PropertySource("classpath:application.properties")
 public class JavaConfig {
 
-//    @Bean
-//    public SimpleDriverDataSource source() {
-//        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-//        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
-//        dataSource.setUsername("root");
-//        dataSource.setPassword("root");
-//        dataSource.setUrl("jdbc:mysql://localhost/webstore");
-//        return dataSource;
-//    }
-//
-//    @Bean
-//    public JdbcTemplate jdbc(SimpleDriverDataSource dataSource) {
-//        return new JdbcTemplate(dataSource);
-//    }
-
-
-    private static final String PROPERTY_NAME_DATABASE_DRIVER = "db.driver";
-    private static final String PROPERTY_NAME_DATABASE_PASSWORD = "db.password";
-    private static final String PROPERTY_NAME_DATABASE_URL = "db.url";
-    private static final String PROPERTY_NAME_DATABASE_USERNAME = "db.username";
-
-    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
-    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
-    private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
-
-    @Resource
-    private Environment env;
-
     @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
-        dataSource.setDriverClassName(env.getRequiredProperty(PROPERTY_NAME_DATABASE_DRIVER));
-        dataSource.setUrl(env.getRequiredProperty(PROPERTY_NAME_DATABASE_URL));
-        dataSource.setUsername(env.getRequiredProperty(PROPERTY_NAME_DATABASE_USERNAME));
-        dataSource.setPassword(env.getRequiredProperty(PROPERTY_NAME_DATABASE_PASSWORD));
-
+    public SimpleDriverDataSource source() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        dataSource.setUrl("jdbc:mysql://localhost/webstore");
         return dataSource;
     }
 
     @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-        sessionFactoryBean.setDataSource(dataSource());
-        sessionFactoryBean.setPackagesToScan(env.getRequiredProperty(PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN));
-        sessionFactoryBean.setHibernateProperties(hibProperties());
-        return sessionFactoryBean;
+    public JdbcTemplate jdbc(SimpleDriverDataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
+//
+//
+//    private static final String PROPERTY_NAME_DATABASE_DRIVER = "db.driver";
+//    private static final String PROPERTY_NAME_DATABASE_PASSWORD = "db.password";
+//    private static final String PROPERTY_NAME_DATABASE_URL = "db.url";
+//    private static final String PROPERTY_NAME_DATABASE_USERNAME = "db.username";
+//
+//    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
+//    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
+//    private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
+//
+//    @Resource
+//    private Environment env;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//
+//        dataSource.setDriverClassName(env.getRequiredProperty(PROPERTY_NAME_DATABASE_DRIVER));
+//        dataSource.setUrl(env.getRequiredProperty(PROPERTY_NAME_DATABASE_URL));
+//        dataSource.setUsername(env.getRequiredProperty(PROPERTY_NAME_DATABASE_USERNAME));
+//        dataSource.setPassword(env.getRequiredProperty(PROPERTY_NAME_DATABASE_PASSWORD));
+//
+//        return dataSource;
+//    }
+//
+//    @Bean
+//    public LocalSessionFactoryBean sessionFactory() {
+//        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+//        sessionFactoryBean.setDataSource(dataSource());
+//        sessionFactoryBean.setPackagesToScan(env.getRequiredProperty(PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN));
+//        sessionFactoryBean.setHibernateProperties(hibProperties());
+//        return sessionFactoryBean;
+//    }
+//
+//    private Properties hibProperties() {
+//        Properties properties = new Properties();
+//        properties.put(PROPERTY_NAME_HIBERNATE_DIALECT, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
+//        properties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
+//        return properties;
+//    }
+//
+//    @Bean
+//    public HibernateTransactionManager transactionManager() {
+//        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+//        transactionManager.setSessionFactory(sessionFactory().getObject());
+//        return transactionManager;
+//    }
+//
+//    @Bean
+//    public UrlBasedViewResolver setupViewResolver() {
+//        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+//        resolver.setPrefix("/WEB-INF/views/");
+//        resolver.setSuffix(".jsp");
+//        resolver.setViewClass(JstlView.class);
+//        return resolver;
+//    } //TODO iserba 17-21.04.2016 make project using Hibernate JPA (16.04 add hibernate properties and configuration to JavaConfig
 
-    private Properties hibProperties() {
-        Properties properties = new Properties();
-        properties.put(PROPERTY_NAME_HIBERNATE_DIALECT, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
-        properties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
-        return properties;
-    }
+
+
+
+
 
     @Bean
-    public HibernateTransactionManager transactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory().getObject());
-        return transactionManager;
-    }
-
-    @Bean
-    public UrlBasedViewResolver setupViewResolver() {
-        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        resolver.setViewClass(JstlView.class);
-        return resolver;
-    } //TODO iserba 17-21.04.2016 make project using Hibernate JPA (16.04 add hibernate properties and configuration to JavaConfig
-
-
-
-
-
-
-    @Bean
-    public ProductDAO productDAO(DataSource dataSource) {
+    public ProductDAO productDAO(SimpleDriverDataSource dataSource) {
         return new ProductDAO(dataSource);
     }
 
