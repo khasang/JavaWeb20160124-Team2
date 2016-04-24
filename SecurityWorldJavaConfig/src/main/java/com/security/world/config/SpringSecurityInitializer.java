88bda885669ada87faac4b999032fbb376d914fe
ilnur serbaev <serbaevilnur@gmail.com>
2016-04-24 17:52:46 +0500

    remove projects not related to Webstore

--- SecurityWorldJavaConfig/src/main/java/com/security/world/config/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.security.world.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-    //do nothing
-}
