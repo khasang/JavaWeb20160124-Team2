ed6fdf1f528f988ca157adc97d53aa974b5d92eb
ilnur <serbaevilnur@gmail.com>
2016-05-12 15:45:40 +0300

    remove column productorder in products table

--- STS/webstore/web/WEB-INF/sql/products.sql
+++ STS/webstore/web/WEB-INF/sql/products.sql
@@ -2,6 +2,5 @@ CREATE TABLE `webstore`.`products` (
   `id` INT NOT NULL,
   `pname` MEDIUMTEXT NOT NULL,
   `productDescription` MEDIUMTEXT NULL,
-  `productorder` LONGTEXT NOT NULL,
   `cost` INT NULL,
   PRIMARY KEY (`id`));
