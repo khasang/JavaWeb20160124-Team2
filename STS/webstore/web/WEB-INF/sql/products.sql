04db7411afee08792366acc7500e9ec629320bda
ilnur serbaev <serbaevilnur@gmail.com>
2016-06-09 23:42:53 +0500

    remove tmp files

--- STS/webstore/web/WEB-INF/sql/products.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `webstore`.`products` (
-  `id` INT NOT NULL,
-  `pname` MEDIUMTEXT NOT NULL,
-  `productDescription` MEDIUMTEXT NULL,
-  `cost` INT NULL,
-  PRIMARY KEY (`id`));
--- STS/webstore/web/WEB-INF/sql/users.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `webstore`.`users` (
-  `id` INT NOT NULL,
-  `login` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `security` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
